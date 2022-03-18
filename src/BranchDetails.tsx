272745ac9369e3a3a7366374863549a0c324d9d7
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2022-03-18 18:57:50 +0500

    feature(challnage2): increase readability

--- src/BranchDetails.tsx
+++ src/BranchDetails.tsx
@@ -1,25 +1,41 @@
 import { StyleSheet, View, Text } from 'react-native';
 import { Branch } from './Branch';
 
-export default function BranchDetails({ branch }: { branch: Branch }) {
+export default function BranchDetails({
+  branch: { ServiceAndFacility, Name, Accessibility },
+}: {
+  branch: Branch;
+}) {
+  const services =
+    ServiceAndFacility && ServiceAndFacility.length > 1
+      ? ServiceAndFacility?.slice(0, -1)?.join(', ') +
+        ' and ' +
+        ServiceAndFacility?.slice(-1)
+      : ServiceAndFacility?.join(', ');
+
+  const accessibilities =
+    Accessibility && Accessibility.length > 1
+      ? Accessibility?.slice(0, -1)?.join(', ') +
+        ' and ' +
+        Accessibility?.slice(-1)
+      : Accessibility?.join(', ');
+
   return (
     <View style={styles.container}>
       <View style={styles.row}>
         <Text style={styles.text}>Branch name:</Text>
-        <Text style={styles.textBold}>{branch.Name}</Text>
+        <Text style={styles.textBold}>{Name}</Text>
       </View>
-      {branch.ServiceAndFacility && (
+      {ServiceAndFacility && (
         <View style={styles.row}>
           <Text style={styles.text}>Services:</Text>
-          <Text style={styles.textBold}>
-            {branch.ServiceAndFacility.join(', ')}
-          </Text>
+          <Text style={styles.textBold}>{services}</Text>
         </View>
       )}
-      {branch.Accessibility && (
+      {Accessibility && (
         <View style={styles.row}>
           <Text style={styles.text}>Accessibility:</Text>
-          <Text style={styles.textBold}>{branch.Accessibility.join(', ')}</Text>
+          <Text style={styles.textBold}>{accessibilities}</Text>
         </View>
       )}
     </View>
